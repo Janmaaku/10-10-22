dc6588f7354359c8b4bfae889f55c8683fc0ae16
Janmaaku <113116149+Janmaaku@users.noreply.github.com>
2022-10-13 17:53:37 +0800

    Add files via upload

---
+++ dollarconverter/main.js
@@ -0,0 +1,21 @@
+const usdFunction = function (){
+   let num1 =  document.getElementById("amount").value;
+     let result = Number (num1) * (58.96) ; 
+    let result_final = parseFloat(result).toFixed(2);
+    return result_final;
+};
+const erdFunction = function (){
+    let num1 =  document.getElementById("amount").value;
+      let result = Number (num1) * (57.17) ; 
+     let result_final = parseFloat(result).toFixed(2);
+     return result_final;
+ };
+ const gbFunction = function (){
+    let num1 =  document.getElementById("amount").value;
+      let result = Number (num1) * (65.23) ; 
+     let result_final = parseFloat(result).toFixed(2);
+     return result_final;
+ };
+
+
+    
